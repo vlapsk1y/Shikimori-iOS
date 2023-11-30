2561690148f6f0f07488edb1ffa6d13e4f271f5a
vlapsk1y <vlapskiy@yandex.ru>
2023-11-30 22:43:18 +0500

    first commit

---
+++ Shikimori/ShikimoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShikimoriApp.swift
+//  Shikimori
+//
+//  Created by Vova on 30.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShikimoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
