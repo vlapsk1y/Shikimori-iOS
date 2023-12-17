f474eceaddf98a4168e6c1fba1e89ea061cdb697
vlapsk1y <vlapskiy@yandex.ru>
2023-12-17 16:19:00 +0500

    Added new icon and made more pleasent loginview and moved FormData to Network

---
+++ Shikimori/Network/FormData.swift
@@ -0,0 +1,38 @@
+//
+//  FormData.swift
+//  Shikimori
+//
+//  Created by vlapsk1y on 17.12.2023.
+//
+
+import Foundation
+
+struct FormData {
+    var boundary: String
+    var contentType: String
+    var data: Data
+    
+    @discardableResult mutating func addField(name: String, value: String) -> Self {
+        let str = "\r\nContent-Disposition: form-data; name=\"\(name)\"\r\n\r\n\(value)\r\n--\(boundary)"
+        data.append(str.data(using: .utf8)!)
+        
+        return self 
+    }
+    
+    @discardableResult mutating func complete() -> Self {
+        data.append("--".data(using: .utf8)!)
+        
+        return self
+    }
+    
+    init() {
+        self.boundary = "ShikimoriSwiftBoundary" /** MARK: надо потом что-нибудь с этим сделать,
+                                                  по-идеи данное значение должно быть рандомным,
+                                                  но из-за каких-то приколов свифта при передаче объекта,
+                                                  какого-то черта здесь значение меняется
+                                                  и не совпадает с первоначальным при иницилизации в APIRouter,
+                                                  в итоге запрос становится инвалидным из-за несоответствия boundary в шапке и теле запроса. **/
+        self.contentType = "multipart/form-data; boundary=" + boundary
+        self.data = Data("--\(boundary)".data(using: .utf8)!)
+    }
+}
--- Shikimori/View/LoginView.swift
+++ Shikimori/View/LoginView.swift
@@ -12,11 +12,18 @@ struct LoginView: View {
     
     var body: some View {
         VStack {
+            Image(uiImage: UIImage(named: "AppIcon")!)
+                .resizable()
+                .frame(width: 240, height: 240)
+                .clipShape(RoundedRectangle(cornerRadius: 40))
+                .padding()
             Button(action: {
                 AuthorizationModel().authInShikimori()
             }, label: {
                 Text("Войти")
-            })
+                    .frame(width: 300)
+            }).buttonStyle(.borderedProminent)
+                .controlSize(.large)
         }
     }
 }
