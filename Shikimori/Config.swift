1dc7243673113c1e22b1a4295cf0bca41df7f9ce
vlapsk1y <vlapskiy@yandex.ru>
2024-01-24 18:51:18 +0500

    Changed .gitignore and hidden API keys

---
+++ Shikimori/Config.swift
@@ -0,0 +1,12 @@
+//
+//  Config.swift
+//  Shikimori
+//
+//  Created by vlapsk1y on 24.01.2024.
+//
+
+import Foundation
+
+let CLIENT_ID: String = ""
+let CLIENT_SECRET: String = ""
+let USER_AGENT: String = "ShikimoriSwift"
--- Shikimori/Const.swift
+++ Shikimori/Const.swift
@@ -5,9 +5,6 @@
 //  Created by vlapsk1y on 01.12.2023.
 //
 
-let CLIENT_ID: String = "1H4FlwBsbLa8kdtSUtqal1rvbEfo4z9FsodKjWsKNB4"
-let CLIENT_SECRET: String = "X8Ohm4A1NPUktNwGIuqOtBtdcTwMvl5YKa6Z-q8b8Kg"
-let USER_AGENT: String = "ShikimoriSwift"
 let DOMAIN_API: String = "shikimori.one"
 let REDIRECT_URI: String = "shikimoriswift://auth"
 let AUTH_URL: String = "https://shikimori.one/oauth/authorize?client_id=1H4FlwBsbLa8kdtSUtqal1rvbEfo4z9FsodKjWsKNB4&redirect_uri=shikimoriswift%3A%2F%2Fauth&response_type=code&scope=user_rates+comments+topics"
